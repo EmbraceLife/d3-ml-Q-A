0a0d272c744ff23fef150452c2ba7d65f0b65983
EmbraceLife <1227561934@qq.com>
2016-07-11 08:00:29 +0800

    2 setups

--- d3-setup.js
+++ d3-setup.js
@@ -1,12 +1,9 @@
-# d3-ml-Q-A
-my learning path's questions and answers collected
+// How to setup env for d3 src anatomy?
+// -------------------
+// [checkout stackoverflow](http://stackoverflow.com/questions/38225993/how-to-setup-node-environment-to-run-es6-codes-involving-syntax-like-import)
 
-### How to setup env for d3 src anatomy?
--------------------
-[checkout stackoverflow](http://stackoverflow.com/questions/38225993/how-to-setup-node-environment-to-run-es6-codes-involving-syntax-like-import)
-
-**5 steps to set it up**
-
+// 5 steps to set it up
+/*
 1. create a directory and run `npm init` at its terminal;
 2. move inside this directory's terminal run:
      `npm install --save-dev PackageName`
@@ -17,22 +14,21 @@ my learning path's questions and answers collected
     run `echo '{ "presets": ["es2015"] }' > .babelrc;`
 5. In the terminal run scripts with:
     `./node_modules/.bin/babel-node script.js`
+*/
 
 
+// How to run docco for all js scripts?
+// -------------------
+// [checkout video guide1](https://www.youtube.com/watch?v=1BEidZzIWjM)      
+// [video guide2](https://vimeo.com/91118854)
 
-### How to run `docco` for all JS scripts?
--------------------
-Online Resources:    
-
-[video guide1](https://www.youtube.com/watch?v=1BEidZzIWjM)      
-[video guide2](https://vimeo.com/91118854)   
-
-** 4 steps to set it up**   
-after the above 5 steps are done, install 3 packages globally with npm:    
-
+// steps to set it up
+/*
+1. after the above 5 steps are done, install 3 packages globally with npm:
 ```
 1. `sudo npm install -g pygments`
 2. `sudo npm install -g coffe-script`
 3. `sudo npm install -g docco`
 4. `docco *.js` to create a folder `docs` with all html files for each js file
 ```
+*/
